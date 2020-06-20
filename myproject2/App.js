ead08af70495a3c989ab660005527385f005fee3
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-20 23:18:30 +0530

    FlatList component

--- myproject2/App.js
+++ myproject2/App.js
@@ -1,37 +1,36 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
 
+  // when object is contained key as well as id do't want to use keyExtractor
   const [people, setPeople] = useState([
-    { name: 'shaun', key: '1' },
-    { name: 'yoshi', key: '2' },
-    { name: 'mario', key: '3' },
-    { name: 'luigi', key: '4' },
-    { name: 'peach', key: '5' },
-    { name: 'toad', key: '6' },
-    { name: 'bowser', key: '7' },
+    { name: 'shaun', userId: '1' },
+    { name: 'yoshi', userId: '2' },
+    { name: 'mario', userId: '3' },
+    { name: 'luigi', userId: '4' },
+    { name: 'peach', userId: '5' },
+    { name: 'toad', userId: '6' },
+    { name: 'bowser', userId: '7' },
   ]);
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {/* {people.map((item) => {
-          return (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          )
-        })} */}
-
-        {/* Instead of using above code use below code */}
-
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.userId}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+      {/* <ScrollView>
         {people.map((item) => (
           <View key={item.key}>
             <Text style={styles.item}>{item.name}</Text>
           </View>
         ))}
-      </ScrollView>
+      </ScrollView> */}
     </View>
   );
 }
@@ -49,6 +48,8 @@ const styles = StyleSheet.create({
     marginTop: 24,
     padding: 30,
     backgroundColor: 'pink',
-    fontSize: 24
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24
   }
 });
