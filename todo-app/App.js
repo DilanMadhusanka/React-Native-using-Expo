a3aac715407c6376eba2de845147ac20c6935674
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-21 10:18:31 +0530

    create dummy code inside App.js

--- todo-app/App.js
+++ todo-app/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy cofee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'plan on the switch', key: '3' },
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* header */}
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,7 +29,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding:40
+  },
+  list: {
+    marginTop: 20
+  }
 });
