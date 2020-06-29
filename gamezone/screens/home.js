2e32af7e58abeae4fbe3a0985541ddc5785e09c1
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-29 23:23:09 +0530

    passing data between screen

--- gamezone/screens/home.js
+++ gamezone/screens/home.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global/'
 
 export default function Home({navigation}) {
 
-    const pressHandler = () => {
-        navigation.navigate('ReviewDetails')
-        // navigation.push('ReviewDetails')
-    }
+    const [reviews, setReviews] = useState([
+        { title: 'Sherklok Homes', rating: 5 , body: 'this is the fiction story', key: '1' },
+        { title: 'The Blood', rating: 4 , body: 'this is the short story', key: '2' },
+        { title: 'Ape Gama', rating: 3 , body: 'this is the novel story', key: '3' }
+    ])
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title='go to review details' onPress={pressHandler} />
+            <FlatList 
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
